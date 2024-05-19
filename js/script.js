c04e1512c7e295aab9dc18f6608cc97a6ab4519e
Daria <scheglova.daria@gmail.com>
2024-05-20 01:43:45 +0300

    add js-folder, gallery array

---
+++ js/script.js
@@ -0,0 +1,38 @@
+gallery_array = [
+  {
+    link: "./assets/images/gallery-1.png",
+    category: "визаж",
+  },
+  {
+    link: "./assets/images/gallery-2.png",
+    category: "визаж",
+  },
+  {
+    link: "./assets/images/gallery-3.png",
+    category: "маникюр",
+  },
+  {
+    link: "./assets/images/gallery-4.png",
+    category: "визаж",
+  },
+  {
+    link: "./assets/images/gallery-5.png",
+    category: "парикмахерские услуги",
+  },
+  {
+    link: "./assets/images/gallery-6.png",
+    category: "визаж",
+  },
+  {
+    link: "./assets/images/gallery-7.png",
+    category: "маникюр",
+  },
+  {
+    link: "./assets/images/gallery-8.png",
+    category: "парикмахерские услуги",
+  },
+  {
+    link: "./assets/images/gallery-9.png",
+    category: "визаж",
+  },
+];
