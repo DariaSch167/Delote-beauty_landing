b90a89378ec74cb143af30759e40a4cf3ad49576
Daria <scheglova.daria@gmail.com>
2024-05-21 23:35:20 +0300

    add gallery script

--- js/script_main-page.js
+++ js/script_main-page.js
@@ -1,3 +1,7 @@
+// MAIN - gallery section - add/filter data functions
+
+const galleryCards = document.querySelector(".gallery__cards");
+
 const galleryData = [
   {
     link: "./assets/images/gallery-1.jpg",
@@ -35,20 +39,22 @@ const galleryData = [
     link: "./assets/images/gallery-9.jpg",
     category: "визаж",
   },
+  {
+    link: "./assets/images/gallery-3.jpg",
+    category: "маникюр",
+  },
+  {
+    link: "./assets/images/gallery-5.jpg",
+    category: "парикмахерские услуги",
+  },
 ];
 
-function galleryFilter(filter) {
-  const galleryDataFiltered = galleryData;
-  galleryDataFiltered.filter((card) => card.category === filter);
-
-  return galleryDataFiltered;
+function filterData(array, key) {
+  galleryCards.innerHTML = "";
+  return (array = array.filter((card) => card.category === `${key}`));
 }
 
-console.log(galleryFilter());
-
-const galleryCards = document.querySelector(".gallery__cards");
-
-galleryData.forEach(function (card) {
+function createCard(card) {
   const galleryCard = document.createElement("div");
   galleryCard.classList.add(".gallery__card");
   galleryCards.append(galleryCard);
@@ -58,4 +64,55 @@ galleryData.forEach(function (card) {
   cardImage.src = card.link;
   cardImage.setAttribute("alt", "card.category");
   galleryCard.append(cardImage);
-});
+}
+
+function fullGalleryData() {
+  galleryCards.innerHTML = "";
+  let galleryFullData = galleryData.slice(0, 9);
+  for (let i = 0; i < galleryFullData.length; i++) {
+    createCard(galleryFullData[i]);
+  }
+}
+
+// Gallery full content
+document.addEventListener("DOMContentLoaded", fullGalleryData);
+
+const btnShowAll = document.getElementById("gallery_show-all");
+btnShowAll.onclick = function () {
+  galleryCards.innerHTML = "";
+  galleryData.forEach(function (card) {
+    createCard(card);
+  });
+};
+
+// Gallery filtered content
+const btnFilterOne = document.getElementById("gallery_filter-1");
+const btnFilterTwo = document.getElementById("gallery_filter-2");
+const btnFilterThree = document.getElementById("gallery_filter-3");
+
+btnFilterOne.onclick = function () {
+  let galleryDataFiltered = JSON.parse(JSON.stringify(galleryData));
+  galleryDataFiltered = filterData(
+    galleryDataFiltered,
+    "парикмахерские услуги"
+  );
+  galleryDataFiltered.forEach(function (card) {
+    createCard(card);
+  });
+};
+
+btnFilterTwo.onclick = function () {
+  let galleryDataFiltered = JSON.parse(JSON.stringify(galleryData));
+  galleryDataFiltered = filterData(galleryDataFiltered, "маникюр");
+  galleryDataFiltered.forEach(function (card) {
+    createCard(card);
+  });
+};
+
+btnFilterThree.onclick = function () {
+  let galleryDataFiltered = JSON.parse(JSON.stringify(galleryData));
+  galleryDataFiltered = filterData(galleryDataFiltered, "визаж");
+  galleryDataFiltered.forEach(function (card) {
+    createCard(card);
+  });
+};
